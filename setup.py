b0a67fecae0d6e0a7dbc04a5754ac46a0435ade0
GilseungAhn <ahn.kilseung@gmail.com>
2021-12-29 16:11:15 +0900

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="qspy",
-    version="0.1.2",
+    version="0.1.3",
     license='MIT',
     author="Gil's LAB",
     author_email="gils_lab@naver.com",
@@ -14,7 +14,9 @@ setuptools.setup(
     package_data={
         'qspy': ['datasets/pickle_data/stock_price/*.pkl',
                  'datasets/pickle_data/finance_state/**/*.pkl',
-                 'datasets/pickle_data/stock_list.xlsx']
+                 'datasets/pickle_data/stock_list.xlsx',
+                 'utils/Code_to_Name.pckl',
+                 'utils/Name_to_Code.pckl']
     },
     classifiers=[
         # 패키지에 대한 태그
