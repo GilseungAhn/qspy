6ebaa249bc425a71f0cfd343cffcb44f826b7a97
GilseungAhn <ahn.kilseung@gmail.com>
2022-01-03 10:17:37 +0900

    update

--- qspy/analysis/__init__.py
+++ qspy/analysis/__init__.py
@@ -2,6 +2,7 @@ from .candle_pattern import bullish_engulfing
 from .candle_pattern import bearish_engulfing
 from .candle_pattern import three_black_crows
 from .candle_pattern import three_white_soldiers
+from .calendar_effect import 
 
 __all__ = [
     "bearish_engulfing",
---
+++ qspy/analysis/calendar_effect.py
@@ -0,0 +1,8 @@
+
+def week_effect(data, buy_weekday, sell_weekday, buy_col, sell_col):
+    pass
+
+
+    return buy_arr, sell_arr,
+
+def
